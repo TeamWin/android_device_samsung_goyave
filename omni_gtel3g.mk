745153c67a1fffa3a1bf3357da736037cefbf511
KrutosX <52695559+KrutosVIP@users.noreply.github.com>
2022-11-18 13:27:00 +0400

    Bring back aosp.mk here.

--- omni_gtel3g.mk
+++ omni_gtel3g.mk
@@ -23,14 +23,13 @@
 #$(call inherit-product, device/samsung/v1awifi/device.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Common path for device trees
 COMMON_PATH := device/samsung/sc8830-common
 
 # Device path for OEM device tree
 DEVICE_PATH := device/samsung/gtel3g
-
-$(call inherit-product, $(COMMON_PATH)/device-common.mk)
 
 PRODUCT_PACKAGES += \
     charger_res_images \
